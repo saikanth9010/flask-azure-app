bcc02db306f0759d43038fe1b0f6548719f21394
saikanth9010 <saikanth9010@gmail.com>
2025-04-07 19:23:26 -0400

    Add basic Flask route tests

---
+++ tests/test_routes.py
@@ -0,0 +1,11 @@
+from app import app
+
+def test_home():
+    response = app.test_client().get('/')
+    assert response.status_code == 200
+    assert b"Welcome" in response.data
+
+def test_about():
+    response = app.test_client().get('/about')
+    assert response.status_code == 200
+    assert b"About" in response.data
